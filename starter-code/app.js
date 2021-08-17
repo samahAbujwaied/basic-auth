57955520a643ad1895912c3d777522370b2f54d0
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-08-17 20:45:35 +0300

    lab 6 test

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-
+const autheModel = require('./models/auth.model');
 const POSTGRES_URI = "postgres://localhost:5432/user";
 let sequelize = new Sequelize(POSTGRES_URI, {});
 
+const AutheModel = autheModel(sequelize, DataTypes);
+
 module.exports={
-  db:sequelize 
+  db:sequelize ,
+  AutheModel
 }
--- starter-code/middlewares/signup.middle.js
+++ starter-code/middlewares/signup.middle.js
@@ -1,25 +1,41 @@
 const bcrypt = require('bcrypt');
 const autheModel = require('../models/auth.model');
+// const { Sequelize, DataTypes } = require('sequelize');
+// let sequelize = new Sequelize("postgres://localhost:5432/user", {});
+// const modelDB = new autheModel(sequelize, DataTypes)
 module.exports = async (req,res,next)=>{
 
   console.log(req.body);
+  console.log('inside try',req.body);
+  req.body.password = await bcrypt.hash(req.body.password, 10);
+  console.log(req.body.password);
+  console.log('inside try------->',req.body);
     try {
-      console.log('inside try',req.body);
+     
         req.body.password = await bcrypt.hash(req.body.password, 10);
-        console.log(req.body.password);
-        console.log('autheModel',autheModel.name);
-        console.log(req.body.username);
-        console.log(req.body.password);
+        
         const record = await autheModel.create({
           username : req.body.username,
           password: req.body.password
         });
         console.log('record',record);
-        res.status(200).json(record);
+        // res.status(200).json(record);
+        next()
       } catch (e) {
         next('error in signup')
         //    res.status(403).send("Error Creating User"); 
     }
+    // try {
+    //   let userRecord = await users.create(req.body);
+    //   console.log('userRecord--------->',userRecord);
+    //   const output = {
+    //     user: userRecord,
+    //     token: userRecord.token
+    //   };
+    //   res.status(201).json(output);
+    // } catch (e) {
+    //   next(e.message)
+    // }
 
 }
 
